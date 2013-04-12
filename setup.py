8a37ff0bcb897e9b65fbb7f2d0a39ed053554f43
Heinz-Peter Lang <lang@weblyzard.com>
2013-04-12 11:30:19 +0200

    moved JeremiaClient2 here, new version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools.command import sdist
 
 setup (
     name = 'weblyzard_api',
-    version = '0.1.1',
+    version = '0.2.0',
     description= ' Web services for weblyzard',
     author = 'Heinz-Peter Lang and Albert Weichselbraun',
     author_email = 'lang@weblyzard.com',
