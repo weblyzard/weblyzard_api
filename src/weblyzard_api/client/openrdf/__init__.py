0e69c2e63789c3e36c05a70f5569769b81520138
Heinz-Peter Lang <lang@weblyzard.com>
2014-02-14 16:14:05 +0100

    chg: refactoring of Rdf client -> support for more functions

---
+++ src/weblyzard_api/client/openrdf/__init__.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+'''
+Created on 19.12.2013
+
+@author: heinz-peterlang
+
+For details: `check Sesame REST API<http://openrdf.callimachus.net/sesame/2.7/docs/system.docbook?view#The_Sesame_REST_HTTP_Protocol>`_
+
+http://www.csee.umbc.edu/courses/graduate/691/spring14/01/examples/sesame/openrdf-sesame-2.6.10/docs/system/ch08.html
+
+'''
+import json
+import requests
+from pprint import pprint
+from collections import namedtuple
+from urllib import urlencode
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+QUERIES = {
+'configured_profiles': ''' PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+        
+        SELECT ?s ?profile_name 
+        WHERE {
+               ?s rdfs:label  ?profile_name .
+        }
+    '''           
+}
+
+RepositoryDetail = namedtuple('RepositoryDetail', ['id', 'uri', 'title'])
+
+class OpenRdfClient(object):
+    
+    DEFAULT_HEADERS = {'Accept': 'application/sparql-results+json'}
+    URL_MAPPING = {'get_repositories': ('/repositories', 'GET'),
+                   'fetch_statements_repository': ('', 'GET')}
+    
+    def __init__(self, server_uri, config_repository='config.weblyzard.com'):
+        ''' initializes the client
+        :param server_uri: URL of the server
+        '''
+        if not 'openrdf-sesame' in server_uri:
+            server_uri = '%s/openrdf-sesame' % server_uri
+        
+        self.server_uri = server_uri
+        self.repository_url_tmplt = self.server_uri + '/repositories/%s'
+        self.config_repository = config_repository
+        
+    def run_query(self, repository_name, query):
+        sparql = SPARQLWrapper(self.repository_url_tmplt % repository_name)
+        sparql.setQuery(query.strip())
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        
+        for result in results['results']['bindings']:
+            yield result
+    
+    def get_profiles(self):
+        ''' returns a dictionary (subject_uri, profile_name) for all profiles
+        configured in the config repositoriy
+        '''
+        self.check_config_repo()
+        
+        query = QUERIES['configured_profiles']
+        profiles = {}
+        
+        for result in self.run_query(self.config_repository, query):
+            profile_name = result['profile_name']['value']
+            subject_uri = result['s']['value']
+            profiles[profile_name] = subject_uri
+    
+        return profiles
+    
+    def update_profile(self, profile_name, profile_definition):
+        ''' Updates the given profile on the server '''
+        
+        profiles = self.get_profiles()
+        
+        if profile_name in profiles: 
+            print profile_name
+            subject_uri = profiles[profile_name]
+            
+            # TODO: check why we need to fix this????
+            if not subject_uri.endswith('>'):
+                subject_uri = '%s>' % subject_uri
+                
+            self.delete_statements(self.config_repository, 
+                                   subj=subject_uri,
+                                   delete=True)
+        self.upload(content=profile_definition, 
+                    context='config.weblyzard.com',
+                    target_repository=self.config_repository)
+        
+    def check_config_repo(self):
+        repositories = self.get_repositories()
+        
+        if not self.config_repository in repositories: 
+            print 'warning config repo "%s" does not exist' % self.config_repository
+        
+    def request(self, function, data=None, params=None, delete=False, 
+                content_type='applicatoni/rdf+json', 
+                accept='application/sparql-results+json'):
+        ''' executes the requests to the TripleStores
+        :param function: function (path) to request
+        :param data: data to add to the POST request
+        :returns: result of the server
+        :rtype: json encoded dict
+        '''
+        print '%s/%s' % (self.server_uri, function)
+        
+        if data: 
+            method = 'POST'
+            headers = {'content-type': content_type}
+        else: 
+            if delete: 
+                method = 'DELETE'
+            else: 
+                method = 'GET'
+            headers = {'Accept': accept}
+            
+            if params:
+                function = '%s?%s' % (function, params)
+    
+        print method, '%s/%s' % (self.server_uri, function)
+    
+        r = requests.request(method, 
+                             '%s/%s' % (self.server_uri, function), 
+                             data=data, 
+                             headers=headers)
+        text = r.text
+        
+        if isinstance(text, unicode):
+            text = text.encode('utf-8')
+        
+        print text
+        
+        return json.loads(r.text) if r.text else r.text
+
+    def get_repo_size(self, repo_id):
+        
+        result = self.request('repositories/%s/size' % repo_id)
+        print 'get_repo_size', result
+        
+    def get_repositories(self):
+        ''' '''
+        
+        result = self.request('repositories')
+        repositories = {}
+        
+        if 'results' in result and 'bindings' in result['results']: 
+            for repo in result['results']['bindings']:
+                repo_id = repo['id']['value']
+                repositories[repo_id]= RepositoryDetail(repo_id,
+                                                        repo['uri']['value'],
+                                                        repo['title']['value'])
+                
+        return repositories
+    
+    def repository_exists(self, repository_name):
+        return repository_name in self.get_repositories()
+    
+    def get_all_statements(self, repository_name):
+        result = self.request('repositories/%s/statements' % repository_name)
+        pprint(result)
+    
+    def delete_statements(self, repository_name, subj=None, pred=None, obj=None, 
+                          delete=False):
+        function = 'repositories/%s/statements' % repository_name
+        
+        params = {}
+        if subj: params['subj'] = subj  
+        if pred: params['pred'] = pred
+        if obj: params['obj'] = obj
+        if params:
+            params = '&'.join(['%s=%s' % (k, v) for k, v in params.iteritems()])
+        else: 
+            params = None
+        
+        return self.request(function, params=params, delete=delete)
+    
+    def upload(self, content, context, target_repository):
+
+        params = 'context=%s' % context
+        function = 'repositories/%s/statements' % target_repository
+        
+        self.request(function, content, params, delete=False, 
+                     content_type='application/x-turtle;charset=UTF-8')
+        
+if __name__ == '__main__':
+    
+    client = OpenRdfClient('http://voyager.srv.weblyzard.net:8080')
+    pprint(client.get_repositories())
+    
+    print client.repository_exists('test')
+    print client.repository_exists('config.weblyzard.com')
+    client.get_all_statements('SYSTEM')
+#    client.get_repo_size('geonames')
+    
---
+++ src/weblyzard_api/client/openrdf/wl_upload_repository.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+'''
+Created on 17.12.2013
+
+@author: heinz-peterlang
+
+Before uploading a new repository, visit the web-interface and create the 
+repository (type: Java Native store). 
+
+ATTENTION: uploading the same dataset multiple times will lead to redundant data.
+
+'''
+import os
+
+from weblyzard_api.client import WEBLYZARD_API_URL
+
+DEFAULT_HEADERS = {'content-type': }
+
+from weblyzard_api.client.openrdf import OpenRdfClient
+
+def get_files(src_directory, file_ext='.nt'):
+    for root, dirs, files in os.walk(src_directory):
+        for fn in files: 
+            if fn.endswith(file_ext):
+                yield os.path.join(src_directory, fn)
+
+def upload_directory(src_directory, repository, graph_name, 
+                     server_url=WEBLYZARD_API_URL, file_ext='.nt'):
+    ''' uploads all files with the correct file extension to the repository '''
+
+    client = OpenRdfClient(server_url)
+
+    for fn in get_files(src_directory=src_directory, file_ext=file_ext):
+        client.upload(open(fn).read(), graph_name, repository)
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    
+    parser = ArgumentParser(description='Uploads triples to an existing '
+                            'Sesame repository. The script will not create '
+                            'the repository, therefore create the repository '
+                            'in the web-interface (type: java native type)')
+    parser.add_argument('--source-dir', dest='source_directory', required=True, 
+                        help='source directory containing nt files')
+    parser.add_argument('--server-url', dest='server_url', 
+                        default=WEBLYZARD_API_URL,
+                        help='url of the tomcat server')
+    parser.add_argument('--repository', required=True, 
+                        help='name of the target repository')
+    parser.add_argument('--graph-name', dest='graph_name', required=True,
+                        help='name of the graph, e.g. http://dbpedia.org or'
+                        ' http://geonames.org')
+    
+    args = parser.parse_args()
+    upload_directory(server_url=args.server_url, 
+                     src_directory=args.src_directory, 
+                     repository=args.repository, 
+                     graph_name=args.graph_name)
