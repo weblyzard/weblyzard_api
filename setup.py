839be815a12799ba230e24909217bd65dbab0515
Heinz-Peter Lang <lang@weblyzard.com>
2014-04-16 14:06:29 +0200

    chg: updated dependency - requires latest eWRT version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup (
     author_email = 'lang@weblyzard.com',
     packages = find_packages('src'),
     package_dir = {'': 'src'},
-    install_requires = ['eWRT>=0.7.11', 
+    install_requires = ['eWRT>=0.7.12', 
                         'requests'],
     scripts = ['src/weblyzard_api/client/openrdf/wl_upload_repository.py',]
 )
