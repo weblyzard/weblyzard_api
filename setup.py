eed12c878c2118b33d4b31e710946abc1354cc91
Heinz-Peter Lang <lang@weblyzard.com>
2014-05-08 11:58:48 +0200

    new: version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools.command import sdist
 
 setup (
     name = 'weblyzard_api',
-    version = '0.4.2',
+    version = '0.4.3',
     description= ' Web services for weblyzard',
     author = 'Heinz-Peter Lang and Albert Weichselbraun',
     author_email = 'lang@weblyzard.com',
