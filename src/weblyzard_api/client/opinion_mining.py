a53b4e5f94c70833f6348bea9ee316c35f9472da
Heinz-Peter Lang <lang@weblyzard.com>
2014-12-15 17:15:23 +0100

    new: client for opinion mining

---
+++ src/weblyzard_api/client/opinion_mining.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on 15.12.2014
+
+'''
+from eWRT.ws.rest import MultiRESTClient
+from weblyzard_api.client import WEBLYZARD_API_URL, WEBLYZARD_API_USER, WEBLYZARD_API_PASS
+
+SERVER_URL_PATH = '/rest/polarity/document'
+
+class OpinionClient(MultiRESTClient):
+    
+    URL_PATH = '/'.join(SERVER_URL_PATH.split('/')[:-1])
+
+    def __init__(self, url=WEBLYZARD_API_URL, 
+                 usr=WEBLYZARD_API_USER, pwd=WEBLYZARD_API_PASS):
+        MultiRESTClient.__init__(self, service_urls=url, user=usr, password=pwd)
+
+    def get_polarity(self, content, content_format):
+        '''
+        Sends the content in the content_format to the opinion mining server
+        to calculate the polarity/sentiment of the content.
+
+        :param content str: The string containing the document to analyze.
+        :param content_format str: The format of the content. Must be 'xml' or
+            'plaintext'
+        :returns: The content (modified, if xml) and the content's overall
+            polarity in a dict with content and polarity as keys.
+        :rtype: dict
+        '''
+        params = {'format': content_format,
+                  'content': content}
+        result = self.request('document', parameters=params,
+                              return_plain=False)
+        return result['polarity'], result['content']
