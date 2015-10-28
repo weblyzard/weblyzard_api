6bf2fee3275e001ca57b71980ff479ce92866fac
Max Goebel <mcgoebel@gmail.com>
2015-10-28 20:23:01 +0100

    new: added package name to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup (
                         'lxml',
                         'requests'],
     dependency_links = ['git+https://github.com/weblyzard/ewrt.git#egg=eWRT-0.9.1.4'],
-zip_safe = False,
+    zip_safe = False,
     scripts = ['src/weblyzard_api/client/openrdf/wl_upload_repository.py',]
 )
