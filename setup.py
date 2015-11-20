7cfa9455a8f708888ebdf81469c4a3a7f0d12982
lucas <lucasgerrand@gmail.com>
2015-11-20 16:07:41 +0100

    increment version for new build

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools.command import sdist
 
 setup (
     name = 'weblyzard_api',
-    version = '0.5.2',
+    version = '0.5.3',
     description= ' Web services for weblyzard',
     author = 'Heinz-Peter Lang and Albert Weichselbraun',
     author_email = 'lang@weblyzard.com',
