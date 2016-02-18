941ee83d265a80330fd99896de073b9658711fed
Rod Michael Coronel <rodmaykel@gmail.com>
2016-02-18 11:20:17 +0100

    upgrade version code.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command import sdist
 
 setup (
     name = 'weblyzard_api',
-    version = '0.5.7',
+    version = '0.5.8',
     description= ' Web services for weblyzard',
     author = 'Albert Weichselbraun, Heinz-Peter Lang, Max Göbels and Philipp Kuntschik',
     author_email = 'weichselbraun@weblyzard.com',
