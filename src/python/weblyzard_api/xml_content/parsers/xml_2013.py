b289745488feac51af5a9f69d699c3266f8ed504
Max Goebel <mcgoebel@gmail.com>
2016-06-01 10:43:57 +0200

    chg: migrated other xml parser unit tests

--- src/python/weblyzard_api/xml_content/parsers/xml_2013.py
+++ src/python/weblyzard_api/xml_content/parsers/xml_2013.py
@@ -5,7 +5,6 @@ Created on 07.04.2014
 
 @author: heinz-peterlang
 '''
-import unittest
 from weblyzard_api.xml_content.parsers import XMLParser
 
 class XML2013(XMLParser):
@@ -38,45 +37,3 @@ class XML2013(XMLParser):
     @classmethod
     def pre_xml_dump(cls, titles, attributes, sentences):
         return attributes, titles + sentences
-
-class TestXML2013(unittest.TestCase):
-    
-    def test(self):
-        xml = '''<wl:page xmlns:wl="http://www.weblyzard.com/wl/2013#" 
-             xmlns:dc="http://purl.org/dc/elements/1.1/" 
-             wl:id="578351358" 
-             dc:format="text/html" 
-             xml:lang="de" 
-             wl:nilsimsa="37345e380610614cc7696ac08ed098e05fa64211755da1d4f525ef4cd762726e">
-        <wl:sentence 
-            wl:pos="NN $( NN APPR ADJA NN VVPP" 
-            wl:id="b42bb3f2cb7ed667ba311811823f37cf" 
-            wl:token="0,20 21,22 23,38 39,42 43,49 50,56 57,64" 
-            wl:sem_orient="0.0" 
-            wl:significance="0.0"
-            wl:is_title="true">
-                <![CDATA[Freihandelsgespr??che - Erleichterungen f??r kleine Firmen geplant]]>
-        </wl:sentence>
-        <wl:annotation
-            wl:key="some.url.com"
-            wl:surfaceForm="this is the text"
-            wl:start="0"
-            wl:end="10"
-            wl:md5sum="b42bb3f2cb7ed667ba311811823f37cf">
-        </wl:annotation></wl:page>'''
-        
-        attributes, sentences, title_annotations, body_annotations = XML2013.parse(xml)
-
-        assert len(attributes) == 4
-        assert len(sentences) == 1
-        assert all(attr in attributes for attr in ('content_id', 'content_type',
-                                                   'lang', 'nilsimsa'))
-        for sent in sentences: 
-            assert 'id' not in sent
-            assert 'md5sum' in sent
-
-if __name__ == '__main__':
-    unittest.main()
-    
-
-
