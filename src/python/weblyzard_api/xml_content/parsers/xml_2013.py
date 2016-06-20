4d377620df1075413904906af7b3dc575d66b683
Max Goebel <mcgoebel@gmail.com>
2016-06-20 23:18:21 +0200

    new: meta data mappings (attributes) new: meta data mapping md5sum (annotation)

--- src/python/weblyzard_api/xml_content/parsers/xml_2013.py
+++ src/python/weblyzard_api/xml_content/parsers/xml_2013.py
@@ -18,10 +18,19 @@ class XML2013(XMLParser):
                            'foaf': 'http://xmlns.com/foaf/0.1/'}
     VERSION = 2013
     ATTR_MAPPING = {'{%s}nilsimsa' % DOCUMENT_NAMESPACES['wl']: 'nilsimsa',
-                    '{%s}format' % DOCUMENT_NAMESPACES['dc']: 'content_type',
-                    '{%s}lang' % DOCUMENT_NAMESPACES['xml']: 'lang',
                     '{%s}id' % DOCUMENT_NAMESPACES['wl']: 'content_id',
-                    '{%s}source' % DOCUMENT_NAMESPACES['dc']: 'source',}
+                    '{%s}lang' % DOCUMENT_NAMESPACES['xml']: 'lang', #kept for legacy
+                    '{%s}format' % DOCUMENT_NAMESPACES['dc']: 'content_type',
+                    '{%s}language' % DOCUMENT_NAMESPACES['dc']: 'language',
+                    '{%s}source' % DOCUMENT_NAMESPACES['dc']: 'source',
+                    '{%s}identifier' % DOCUMENT_NAMESPACES['dc']: 'url',
+                    '{%s}license' % DOCUMENT_NAMESPACES['dc']: 'license',
+                    '{%s}creator' % DOCUMENT_NAMESPACES['dc']: 'creator',
+                    '{%s}publisher' % DOCUMENT_NAMESPACES['dc']: 'publisher',
+                    '{%s}accountName' % DOCUMENT_NAMESPACES['foaf']: 'user_name',
+                    '{%s}num_views' % DOCUMENT_NAMESPACES['sioc']: 'view_count',
+                    '{%s}num_replies' % DOCUMENT_NAMESPACES['sioc']: 'comment_count',
+                    }
     SENTENCE_MAPPING = {'{%s}token' % DOCUMENT_NAMESPACES['wl']: 'token',
                         '{%s}sem_orient' % DOCUMENT_NAMESPACES['wl']: 'sem_orient',
                         '{%s}significance' % DOCUMENT_NAMESPACES['wl']: 'significance',
@@ -34,7 +43,8 @@ class XML2013(XMLParser):
                           '{%s}start' % DOCUMENT_NAMESPACES['wl']: 'start',
                           '{%s}end' % DOCUMENT_NAMESPACES['wl']: 'end',
                           '{%s}annotationType' % DOCUMENT_NAMESPACES['wl']: 'annotation_type',
-                          '{%s}preferredName' % DOCUMENT_NAMESPACES['wl']: 'preferredName'}
+                          '{%s}preferredName' % DOCUMENT_NAMESPACES['wl']: 'preferredName',
+                          '{%s}md5sum' % DOCUMENT_NAMESPACES['wl']: 'md5sum'}
     FEATURE_MAPPING = {'{%s}key' % DOCUMENT_NAMESPACES['wl']: 'key',
                        '{%s}context' % DOCUMENT_NAMESPACES['wl']: 'context'}
     RELATION_MAPPING = {'{%s}key' % DOCUMENT_NAMESPACES['wl']: 'key'}
