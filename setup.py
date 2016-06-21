b104addc043d67cac7d3cc9ceddb777e36a0d63e
Max Goebel <mcgoebel@gmail.com>
2016-06-21 10:02:55 +0200

    chg: fixed version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command import sdist
 
 setup (
     name = 'weblyzard_api',
-    version = '0.7.20160621',
+    version = '0.7.2016.06.21',
     description= ' Web services for weblyzard',
     author = 'Albert Weichselbraun, Heinz-Peter Lang, Max Göbel and Philipp Kuntschik',
     author_email = 'weichselbraun@weblyzard.com',
