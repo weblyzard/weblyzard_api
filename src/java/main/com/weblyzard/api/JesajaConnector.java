59e108956f592d39052419423ba9c58f7c9d7644
Philipp Kuntschik <Philipp.Kuntschik@HTWChur.ch>
2016-10-07 16:56:23 +0200

    wip: JesajaConnector

---
+++ src/java/main/com/weblyzard/api/JesajaConnector.java
@@ -0,0 +1,31 @@
+package com.weblyzard.api;
+
+public class JesajaConnector extends BasicConnector {
+
+	/**
+	 * @see BasicConnector
+	 */
+	public JesajaConnector() {
+		super();
+	}
+
+
+
+	/**
+	 * @see BasicConnector
+	 */
+	public JesajaConnector(String weblyzard_url) {
+		super(weblyzard_url);
+	}
+
+
+
+	/**
+	 * @see BasicConnector
+	 */
+	public JesajaConnector(String weblyzard_url, String username, String password) {
+		super(weblyzard_url, username, password);
+	}
+	
+	
+}
