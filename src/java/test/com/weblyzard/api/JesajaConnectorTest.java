f3afd652b6b51f07fe7ea184277511cb3050dbe8
Philipp Kuntschik <Philipp.Kuntschik@HTWChur.ch>
2016-10-07 17:33:43 +0200

    new: support for Jesaja Webservices (UNTESTED)

---
+++ src/java/test/com/weblyzard/api/JesajaConnectorTest.java
@@ -0,0 +1,18 @@
+package com.weblyzard.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class JesajaConnectorTest {
+
+	// TODO:implement
+
+	@Test
+	@Ignore
+	public void notImplemented() {
+		fail("not implemented");
+	}
+
+}
