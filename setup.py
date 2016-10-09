dfd6d8adbd0ac1c75fdfd5888fd036dadbe089d2
Max Goebel <mcgoebel@gmail.com>
2016-10-10 00:05:20 +0200

    chg: upped version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command import sdist
 
 setup (
     name = 'weblyzard_api',
-    version = '0.7.20161006',
+    version = '0.7.20161008',
     description= ' Web services for weblyzard',
     author = 'Albert Weichselbraun, Heinz-Peter Lang, Max Göbel and Philipp Kuntschik',
     author_email = 'weichselbraun@weblyzard.com',
