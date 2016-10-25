ef29b62adb02fcff640d55f76110f703c46ecd46
Stefan Gindl <stefan.gindl@modul.ac.at>
2016-10-25 10:54:10 +0200

    add: finished the skb rest client

--- src/python/weblyzard_api/client/skb_rest_client.py
+++ src/python/weblyzard_api/client/skb_rest_client.py
@@ -5,26 +5,20 @@ Created on Oct 24, 2016
 '''
 
 import requests
+import unittest
+import urllib
+
 
 class SKBRESTClient():
     
-    PATH = '1.0/skb/translation?'
+    TRANSLATION_PATH = '1.0/skb/translation?'
 
     def __init__(self, url, usr=None, pwd=None):
         '''
-        :param url: URL of the jeremia web service
-        :param usr: optional user name
-        :param pwd: optional password
+        :param url: URL of the SKB web service
         '''
         self.url = url
-    def translate(self, client_name, text, source_language, target_language):
-#         url = ('http://localhost:5002/'
-        url = ('%s/%s'
-               'client=%s&'
-               'term=%s&'
-               'source=%s&'
-               'target=%s' % (client_name, text, source_language, target_language))
-        response = requests.get(url)
-        print('response.text:', response.text)
-        return(response.text, target_language)
-        
+
+    def translate(self, **kwargs):
+        response = requests.get('%s/%s' % (self.url, self.TRANSLATION_PATH), params=kwargs)
+        return(response.text, kwargs['target'])
