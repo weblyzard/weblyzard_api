e9ad73ea6c77e7449632aa86cf6990d7f4a82dcc
Albert Weichselbraun <albert@weichselbraun.net>
2017-08-24 00:10:59 +0200

    add: additional unittests for MD5Digest

--- src/java/test/com/weblyzard/api/datatype/MD5DigestTest.java
+++ src/java/test/com/weblyzard/api/datatype/MD5DigestTest.java
@@ -2,14 +2,94 @@ package com.weblyzard.api.datatype;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javax.xml.bind.DatatypeConverter;
 import org.junit.Test;
 
 public class MD5DigestTest {
+
+    private final int SAMPLE_SIZE = 1000;
+
+    @Test
+    public void testFromHexDigest() {
+        final String text = "Ringstrasse 34, 7000 Chur";
+        MD5Digest m = MD5Digest.fromText(text);
+        MD5Digest mFromDigest = MD5Digest.fromHexDigest("34acdf2d3df92b5e360159f2f664a9e4");
+        assertEquals(m, mFromDigest);
+        assertEquals(m.toString(), mFromDigest.toString());
+    }
+
+    @Test
+    public void testFromText() {
+        final String text = "Ringstrasse 34, 7000 Chur";
+        MD5Digest m = MD5Digest.fromText(text);
+        String md5string =
+                DatatypeConverter.printHexBinary(
+                        MD5Digest.getMessageDigest().digest(text.getBytes()));
+        assertEquals(md5string.toLowerCase(), m.toString());
+    }
+
+    @Test
+    public void testEqualsObject() {
+        Random rnd = new Random();
+        // generate test data
+        System.out.println("Generating random testdata.");
+        List<String> testData = new ArrayList<>(SAMPLE_SIZE);
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            testData.add("" + rnd.nextLong());
+        }
+        long time;
+
+        System.out.println("Comparing test data with Strings");
+        time = System.nanoTime();
+        List<String> digestString = new ArrayList<>(SAMPLE_SIZE);
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            digestString.add(
+                    DatatypeConverter.printHexBinary(
+                                    MD5Digest.getMessageDigest().digest(testData.get(i).getBytes()))
+                            .toLowerCase());
+        }
+
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            for (int j = 0; j < SAMPLE_SIZE; j++) {
+                if (testData.get(i) != testData.get(j)) {
+                    assertNotEquals(digestString.get(i), digestString.get(j));
+                } else {
+                    assertEquals(digestString.get(i), digestString.get(j));
+                }
+            }
+        }
+        System.out.println("Time Required " + (System.nanoTime() - time));
+
+        System.out.println("Comparing test data with MD5Digest");
+        time = System.nanoTime();
+        List<MD5Digest> digestData = new ArrayList<>(SAMPLE_SIZE);
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            digestData.add(MD5Digest.fromText(testData.get(i)));
+        }
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            for (int j = 0; j < SAMPLE_SIZE; j++) {
+                if (testData.get(i) != testData.get(j)) {
+                    assertNotEquals(digestData.get(i), digestData.get(j));
+                } else {
+                    assertEquals(digestData.get(i), digestData.get(j));
+                }
+            }
+        }
+        System.out.println("Time Required " + (System.nanoTime() - time));
+
+        // ensure that the string representation of the computed
+        // md5sums are identical
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            assertEquals(digestData.get(i).toString(), digestString.get(i));
+        }
+    }
 
     @Test
     public void testCompare() {
