33e29abfda3215f1adad636df9202cfb78b3700d
Max Goebel <mcgoebel@gmail.com>
2018-05-16 10:33:41 +0200

    chg: pep8

--- src/python/weblyzard_api/client/openrdf/queries.py
+++ src/python/weblyzard_api/client/openrdf/queries.py
@@ -5,35 +5,35 @@ Created on 14.02.2014
 '''
 from weblyzard_api.client.openrdf import OpenRdfClient
 
+
 class SPARQLQuery(object):
-    
+
     def __init__(self, repository):
-        
+
         pass
-    
+
 
 QUERIES = {
-'configured_profiles': 
+    'configured_profiles':
     '''
         PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
         SELECT ?s ?profile_name 
         WHERE {
                ?s rdfs:label  ?profile_name .
         }
-    '''           
+    '''
 }
 
 if __name__ == '__main__':
     server_uri = ''
     repository_name = 'config.weblyzard.com'
-    
+
     query = '''
     
     '''
-    
+
     client = OpenRdfClient(server_uri)
-    
-    
+
     '''
             DELETE DATA
             {
@@ -41,13 +41,12 @@ if __name__ == '__main__':
             }
             
     '''
-    
+
     '''
     SELECT * WHERE 
             {
                 <http://www.weblyzard.com/recognize/lexicon/en.off.cities.500000> ?p ?o
             }
     '''
-    
-    
+
     print(client.get_profiles())
