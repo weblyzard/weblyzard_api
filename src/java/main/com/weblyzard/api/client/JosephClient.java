959499f1e3ba890224fa2ce67b1745d53b001fc2
Philipp Kuntschik <Philipp.Kuntschik@htwchur.ch>
2018-06-07 09:45:13 +0200

    adapted the joseph client to the new document format

--- src/java/main/com/weblyzard/api/client/JosephClient.java
+++ src/java/main/com/weblyzard/api/client/JosephClient.java
@@ -1,13 +1,13 @@
 package com.weblyzard.api.client;
 
-import com.weblyzard.api.model.document.LegacyDocument;
-import com.weblyzard.api.model.recognyze.RecognyzeResult;
 import java.util.List;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import com.weblyzard.api.model.document.Document;
+import com.weblyzard.api.model.recognyze.RecognyzeResult;
 
 /** @author philipp.kuntschik@htwchur.ch */
 public class JosephClient extends BasicClient {
@@ -46,11 +46,9 @@ public class JosephClient extends BasicClient {
     }
 
     public boolean loadProfile(String profileName) {
-        Response response =
-                super.getTarget(LOAD_PROFILE_SERVICE_URL)
-                        .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
-                        .request(MediaType.APPLICATION_JSON_TYPE)
-                        .get();
+        Response response = super.getTarget(LOAD_PROFILE_SERVICE_URL)
+                .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
+                .request(MediaType.APPLICATION_JSON_TYPE).get();
 
         super.checkResponseStatus(response);
         boolean result = response.readEntity(Boolean.class);
@@ -60,11 +58,9 @@ public class JosephClient extends BasicClient {
     }
 
     public boolean cleanProfile(String profileName) {
-        Response response =
-                super.getTarget(CLEAN_PROFILE_SERVICE_URL)
-                        .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
-                        .request(MediaType.APPLICATION_JSON_TYPE)
-                        .get();
+        Response response = super.getTarget(CLEAN_PROFILE_SERVICE_URL)
+                .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
+                .request(MediaType.APPLICATION_JSON_TYPE).get();
 
         super.checkResponseStatus(response);
         boolean result = response.readEntity(Boolean.class);
@@ -74,11 +70,9 @@ public class JosephClient extends BasicClient {
     }
 
     public boolean unloadProfile(String profileName) {
-        Response response =
-                super.getTarget(UNLOAD_PROFILE_SERVICE_URL)
-                        .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
-                        .request(MediaType.APPLICATION_JSON_TYPE)
-                        .get();
+        Response response = super.getTarget(UNLOAD_PROFILE_SERVICE_URL)
+                .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
+                .request(MediaType.APPLICATION_JSON_TYPE).get();
 
         super.checkResponseStatus(response);
         boolean result = response.readEntity(Boolean.class);
@@ -87,13 +81,11 @@ public class JosephClient extends BasicClient {
         return result;
     }
 
-    public boolean train(String profileName, LegacyDocument document, String category) {
-        Response response =
-                super.getTarget(TRAIN_SERVICE_URL)
-                        .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
-                        .resolveTemplate(TEMPLATE_CATEGORY, category)
-                        .request(MediaType.APPLICATION_JSON_TYPE)
-                        .post(Entity.json(document));
+    public boolean train(String profileName, Document document, String category) {
+        Response response = super.getTarget(TRAIN_SERVICE_URL)
+                .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
+                .resolveTemplate(TEMPLATE_CATEGORY, category)
+                .request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(document));
 
         super.checkResponseStatus(response);
         boolean result = response.readEntity(Boolean.class);
@@ -102,13 +94,11 @@ public class JosephClient extends BasicClient {
         return result;
     }
 
-    public boolean retrain(String profileName, LegacyDocument document, String category) {
-        Response response =
-                super.getTarget(RETRAIN_SERVICE_URL)
-                        .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
-                        .resolveTemplate(TEMPLATE_CATEGORY, category)
-                        .request(MediaType.APPLICATION_JSON_TYPE)
-                        .post(Entity.json(document));
+    public boolean retrain(String profileName, Document document, String category) {
+        Response response = super.getTarget(RETRAIN_SERVICE_URL)
+                .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
+                .resolveTemplate(TEMPLATE_CATEGORY, category)
+                .request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(document));
 
         super.checkResponseStatus(response);
         boolean result = response.readEntity(Boolean.class);
@@ -117,13 +107,11 @@ public class JosephClient extends BasicClient {
         return result;
     }
 
-    public boolean forget(String profileName, LegacyDocument document, String category) {
-        Response response =
-                super.getTarget(FORGET_SERVICE_URL)
-                        .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
-                        .resolveTemplate(TEMPLATE_CATEGORY, category)
-                        .request(MediaType.APPLICATION_JSON_TYPE)
-                        .post(Entity.json(document));
+    public boolean forget(String profileName, Document document, String category) {
+        Response response = super.getTarget(FORGET_SERVICE_URL)
+                .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
+                .resolveTemplate(TEMPLATE_CATEGORY, category)
+                .request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(document));
 
         super.checkResponseStatus(response);
         boolean result = response.readEntity(Boolean.class);
@@ -132,21 +120,17 @@ public class JosephClient extends BasicClient {
         return result;
     }
 
-    public List<RecognyzeResult> classify(String profileName, LegacyDocument request) {
+    public List<RecognyzeResult> classify(String profileName, Document request) {
         return this.classify(profileName, request, 0, false);
     }
 
-    public List<RecognyzeResult> classify(
-            String profileName, LegacyDocument request, int limit, boolean withFeatures)
-            throws WebApplicationException {
+    public List<RecognyzeResult> classify(String profileName, Document request, int limit,
+            boolean withFeatures) throws WebApplicationException {
 
-        Response response =
-                super.getTarget(CLASSIFY_SERVICE_URL)
-                        .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName)
-                        .queryParam(PARAM_LIMIT, limit)
-                        .queryParam(PARAM_WITH_FEATURES, withFeatures)
-                        .request(MediaType.APPLICATION_JSON_TYPE)
-                        .post(Entity.json(request));
+        Response response = super.getTarget(CLASSIFY_SERVICE_URL)
+                .resolveTemplate(TEMPLATE_PROFILE_NAME, profileName).queryParam(PARAM_LIMIT, limit)
+                .queryParam(PARAM_WITH_FEATURES, withFeatures)
+                .request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(request));
 
         super.checkResponseStatus(response);
         List<RecognyzeResult> result =
--- src/java/test/com/weblyzard/api/client/integration/JosephClientIT.java
+++ src/java/test/com/weblyzard/api/client/integration/JosephClientIT.java
@@ -1,26 +1,25 @@
 package com.weblyzard.api.client.integration;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
-
+import java.io.IOException;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.weblyzard.api.client.JosephClient;
-import com.weblyzard.api.model.document.LegacyDocument;
-import java.io.IOException;
-import java.util.List;
-import org.junit.Before;
-import org.junit.Test;
+import com.weblyzard.api.model.document.Document;
 
 public class JosephClientIT extends TestClientBase {
 
     private static final String PSALMS_DOCS_WEBLYZARDFORMAT_JSON =
             "resources/psalms-docs-weblyzardformat.json";
 
-    public List<LegacyDocument> psalmDocs;
+    public List<Document> psalmDocs;
 
     private JosephClient client;
 
@@ -41,17 +40,16 @@ public class JosephClientIT extends TestClientBase {
         psalmDocs.stream().forEach(document -> client.train("smc", document, "category"));
     }
 
-    public List<LegacyDocument> readWeblyzardDocuments() {
+    public List<Document> readWeblyzardDocuments() {
         try {
 
             logger.info(JoelClientIT.class.getClassLoader().getResource(".").getPath());
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
             return objectMapper.readValue(
-                    JoelClientIT.class
-                            .getClassLoader()
+                    JoelClientIT.class.getClassLoader()
                             .getResourceAsStream(PSALMS_DOCS_WEBLYZARDFORMAT_JSON),
-                    new TypeReference<List<LegacyDocument>>() {});
+                    new TypeReference<List<Document>>() {});
         } catch (JsonParseException e1) {
             e1.printStackTrace();
         } catch (JsonMappingException e1) {
