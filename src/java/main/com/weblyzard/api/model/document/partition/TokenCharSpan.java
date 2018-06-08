53dec7a55738ed4e54f87cee4b3a3a931037af84
Philipp Kuntschik <Philipp.Kuntschik@htwchur.ch>
2018-06-08 13:27:28 +0200

    chg: return TokenCharSpans instead of CharSpans for setter

--- src/java/main/com/weblyzard/api/model/document/partition/TokenCharSpan.java
+++ src/java/main/com/weblyzard/api/model/document/partition/TokenCharSpan.java
@@ -3,20 +3,33 @@ package com.weblyzard.api.model.document.partition;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 /**
  * A {@link CharSpan} used for token which contains POS and Dependency information.
+ * 
  * @author Albert Weichselbraun
  *
  */
 @NoArgsConstructor
-public class TokenCharSpan extends CharSpan{
+@Accessors(chain = true)
+public class TokenCharSpan extends CharSpan {
     private @Setter @Getter String pos;
     private @Setter @Getter Dependency dependency;
-    
+
     public TokenCharSpan(int start, int end, String pos, Dependency dependency) {
         super(start, end);
         this.pos = pos;
         this.dependency = dependency;
+    }
+
+    @Override
+    public TokenCharSpan setStart(int start) {
+        return (TokenCharSpan) super.setStart(start);
+    }
+
+    @Override
+    public TokenCharSpan setEnd(int end) {
+        return (TokenCharSpan) super.setEnd(end);
     }
 }
