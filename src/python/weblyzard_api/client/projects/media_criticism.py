b56d4b39ba7fac53d86af62b7c4e822131536abe
max <mcgoebel@gmail.com>
2018-11-13 17:13:44 +0100

    new: updated media_criticism client to support document-rewrite

--- src/python/weblyzard_api/client/projects/media_criticism.py
+++ src/python/weblyzard_api/client/projects/media_criticism.py
@@ -4,36 +4,38 @@ Created on Jan 16, 2013
 
 @author: Philipp Kuntschik <philipp.kuntschik@htwchur.ch>
 '''
-from eWRT.ws.rest import  MultiRESTClient
+from eWRT.ws.rest import MultiRESTClient
 
-from weblyzard_api.client import WEBLYZARD_API_URL, WEBLYZARD_API_USER, WEBLYZARD_API_PASS
+from weblyzard_api.client import (
+    WEBLYZARD_API_URL, WEBLYZARD_API_USER, WEBLYZARD_API_PASS)
+
 
 class MediaCriticism(MultiRESTClient):
 
     '''
     base bath to the deployed media criticism mission control
     '''
-    CLASSIFIER_WS_BASE_PATH = '/mc2/rest/'
+    CLASSIFIER_WS_BASE_PATH = '/rest/'
 
-    def __init__(self, url=WEBLYZARD_API_URL, usr=WEBLYZARD_API_USER, 
+    def __init__(self, url=WEBLYZARD_API_URL, usr=WEBLYZARD_API_USER,
                  pwd=WEBLYZARD_API_PASS, default_timeout=None):
         MultiRESTClient.__init__(self, service_urls=url, user=usr, password=pwd,
                                  default_timeout=default_timeout)
 
-    def hello_world(self):
+    def status(self):
         '''
         tests the simple hello world service
         '''
-        return self.request(self.CLASSIFIER_WS_BASE_PATH + 'status') 
+        return self.request(self.CLASSIFIER_WS_BASE_PATH + 'status')
 
-    def check_domain_relevance(self, weblyzard_xml):
+    def check_domain_relevance(self, api_document):
         '''
-        ::param weblyzard_xml:
-            the weblyzard xml to check
+        ::param api_document:
+            the api_document to check
         ::returns
              a tuple which is composed as follows
                (is_relevant, mediacriticism_score, num_recognized_entities)
         '''
-        result = self.request(self.CLASSIFIER_WS_BASE_PATH 
-            + 'checkDocumentRelevance', {'xml_document': weblyzard_xml})
-        return result['relevantDocument'], result['mediacriticism'], result['recognizeEntities']
+        result = self.request(self.CLASSIFIER_WS_BASE_PATH
+                              + 'checkDocumentRelevance', {'document': api_document})
+        return result['relevantDocument'], result['mediacriticism']
