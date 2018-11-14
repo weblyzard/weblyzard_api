4b2c9be5cbef2e41d12baa027653defa9851d9ae
max <mcgoebel@gmail.com>
2018-11-14 10:30:12 +0100

    chg: docstring

--- src/python/weblyzard_api/client/projects/media_criticism.py
+++ src/python/weblyzard_api/client/projects/media_criticism.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Created on Jan 16, 2013
 
 @author: Philipp Kuntschik <philipp.kuntschik@htwchur.ch>
-'''
+"""
 from eWRT.ws.rest import MultiRESTClient
 
 from weblyzard_api.client import (
@@ -12,9 +12,7 @@ from weblyzard_api.client import (
 
 class MediaCriticism(MultiRESTClient):
 
-    '''
-    base bath to the deployed media criticism mission control
-    '''
+    # base bath to the deployed media criticism mission control
     CLASSIFIER_WS_BASE_PATH = '/rest/'
 
     def __init__(self, url=WEBLYZARD_API_URL, usr=WEBLYZARD_API_USER,
@@ -23,19 +21,20 @@ class MediaCriticism(MultiRESTClient):
                                  default_timeout=default_timeout)
 
     def status(self):
-        '''
-        tests the simple hello world service
-        '''
+        """
+        Return the status of the service.
+        """
         return self.request(self.CLASSIFIER_WS_BASE_PATH + 'status')
 
     def check_domain_relevance(self, api_document):
-        '''
-        ::param api_document:
+        """
+        Check the domain relevance of a given document.
+        :param api_document:
             the api_document to check
-        ::returns
+        :returns
              a tuple which is composed as follows
                (is_relevant, mediacriticism_score, num_recognized_entities)
-        '''
+        """
         result = self.request(self.CLASSIFIER_WS_BASE_PATH
                               + 'checkDocumentRelevance', api_document)
         return result['relevantDocument'], result['mediacriticism']
