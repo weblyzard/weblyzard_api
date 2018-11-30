a783b27b7aafbd8471fd17ce68e496606c59a4b5
Sandro Hörler <sandro.hoerler@htwchur.ch>
2018-11-30 14:09:47 +0100

    added javadoc to johndoeDocument

--- src/java/main/com/weblyzard/api/model/johndoe/JohnDoeDocument.java
+++ src/java/main/com/weblyzard/api/model/johndoe/JohnDoeDocument.java
@@ -3,20 +3,33 @@ package com.weblyzard.api.model.johndoe;
 import java.util.List;
 import java.util.Map;
 
-
+/**
+ * The {@link JohnDoeDocument} class is used to represent JohnDoeDocuments.
+ * 
+ * @author sandro.hoerler@htwchur.ch
+ *
+ */
 public class JohnDoeDocument {
 	private String profileName;
 	private String baseUrl;
 	private List<String> names;
-	private Map<String,String> nameAnnonIdMap;
-	
+	private Map<String, String> nameAnnonIdMap;
+
+	/**
+	 * Default constructor for {@link JohnDoeDocument}
+	 * {@link profileName}} {@link baseUrl} and {@link names} must be set to get document processed by johndoe
+	 * @param profileName 
+	 * @param baseUrl
+	 * @param names
+	 */
 	public JohnDoeDocument(String profileName, String baseUrl, List<String> names) {
 		this.profileName = profileName;
 		this.baseUrl = baseUrl;
 		this.names = names;
 	}
-	
-	public JohnDoeDocument() {}
+
+	public JohnDoeDocument() {
+	}
 
 	public String getProfileName() {
 		return profileName;
@@ -49,5 +62,5 @@ public class JohnDoeDocument {
 	public void setNameAnnonIdMap(Map<String, String> nameAnnonIdMap) {
 		this.nameAnnonIdMap = nameAnnonIdMap;
 	};
-	
+
 }
