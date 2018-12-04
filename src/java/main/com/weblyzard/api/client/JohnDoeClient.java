021393ca052155b92868772a7989d7117a050733
Sandro Hörler <sandro.hoerler@htwchur.ch>
2018-12-04 10:35:55 +0100

    fixed style, added common formatter

--- src/java/main/com/weblyzard/api/client/JohnDoeClient.java
+++ src/java/main/com/weblyzard/api/client/JohnDoeClient.java
@@ -3,7 +3,6 @@ package com.weblyzard.api.client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import com.weblyzard.api.model.johndoe.JohnDoeDocument;
 
 /**
@@ -12,28 +11,26 @@ import com.weblyzard.api.model.johndoe.JohnDoeDocument;
  * @author Sandro Hörler
  */
 public class JohnDoeClient extends BasicClient {
-	private static final String PROFILE = "profileName";
-	private static final String CONTENT = "content";
-	private static final String BASEURL = "baseurl";
-	private static final String ANNON_SERVICE_URL = "/rest/annon";
+    private static final String ANNON_SERVICE_URL = "/rest/annon";
 
-	public JohnDoeClient(WebserviceClientConfig c) {
-		super(c, "/johndoe");
-	}
+    public JohnDoeClient(WebserviceClientConfig c) {
+        super(c, "/johndoe");
+    }
 
-	/**
-	 * Computes an anonymized identifier for the given content.
-	 * 
-	 * @param content     the content to assess
-	 * @param profileName the profile name
-	 * @param baseUrl     used for grounding
-	 * @return anonymized identifier
-	 */
-	public JohnDoeDocument annonymizeContent(JohnDoeDocument document) {
-		try (Response response = super.getTarget(ANNON_SERVICE_URL).request(MediaType.APPLICATION_JSON)
-				.post(Entity.entity(document, MediaType.APPLICATION_JSON))) {
-			super.checkResponseStatus(response);
-			return response.readEntity(JohnDoeDocument.class);
-		}
-	}
+    /**
+     * Computes an anonymized identifier for the given content.
+     * 
+     * @param content the content to assess
+     * @param profileName the profile name
+     * @param baseUrl used for grounding
+     * @return anonymized identifier
+     */
+    public JohnDoeDocument annonymizeContent(JohnDoeDocument document) {
+        try (Response response =
+                super.getTarget(ANNON_SERVICE_URL).request(MediaType.APPLICATION_JSON)
+                        .post(Entity.entity(document, MediaType.APPLICATION_JSON))) {
+            super.checkResponseStatus(response);
+            return response.readEntity(JohnDoeDocument.class);
+        }
+    }
 }
--- src/java/main/com/weblyzard/api/model/johndoe/JohnDoeDocument.java
+++ src/java/main/com/weblyzard/api/model/johndoe/JohnDoeDocument.java
@@ -10,57 +10,58 @@ import java.util.Map;
  *
  */
 public class JohnDoeDocument {
-	private String profileName;
-	private String baseUrl;
-	private List<String> names;
-	private Map<String, String> nameAnnonIdMap;
+    private String profileName;
+    private String baseUrl;
+    private List<String> names;
+    private Map<String, String> nameAnnonIdMap;
 
-	/**
-	 * Default constructor for {@link JohnDoeDocument}
-	 * {@link profileName}} {@link baseUrl} and {@link names} must be set to get document processed by johndoe
-	 * @param profileName 
-	 * @param baseUrl
-	 * @param names
-	 */
-	public JohnDoeDocument(String profileName, String baseUrl, List<String> names) {
-		this.profileName = profileName;
-		this.baseUrl = baseUrl;
-		this.names = names;
-	}
+    /**
+     * 
+     * Default constructor for {@link JohnDoeDocument} {@link profileName}} {@link baseUrl} and
+     * {@link names} must be set to get document processed by johndoe.
+     * 
+     * @param profileName Profile name 
+     * @param baseUrl Url of document
+     * @param names extracted names for annonymization
+     */
+    public JohnDoeDocument(String profileName, String baseUrl, List<String> names) {
+        this.profileName = profileName;
+        this.baseUrl = baseUrl;
+        this.names = names;
+    }
 
-	public JohnDoeDocument() {
-	}
+    public JohnDoeDocument() {}
 
-	public String getProfileName() {
-		return profileName;
-	}
+    public String getProfileName() {
+        return profileName;
+    }
 
-	public void setProfileName(String profileName) {
-		this.profileName = profileName;
-	}
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
 
-	public String getBaseUrl() {
-		return baseUrl;
-	}
+    public String getBaseUrl() {
+        return baseUrl;
+    }
 
-	public void setBaseUrl(String baseUrl) {
-		this.baseUrl = baseUrl;
-	}
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
 
-	public List<String> getNames() {
-		return names;
-	}
+    public List<String> getNames() {
+        return names;
+    }
 
-	public void setNames(List<String> names) {
-		this.names = names;
-	}
+    public void setNames(List<String> names) {
+        this.names = names;
+    }
 
-	public Map<String, String> getNameAnnonIdMap() {
-		return nameAnnonIdMap;
-	}
+    public Map<String, String> getNameAnnonIdMap() {
+        return nameAnnonIdMap;
+    }
 
-	public void setNameAnnonIdMap(Map<String, String> nameAnnonIdMap) {
-		this.nameAnnonIdMap = nameAnnonIdMap;
-	};
+    public void setNameAnnonIdMap(Map<String, String> nameAnnonIdMap) {
+        this.nameAnnonIdMap = nameAnnonIdMap;
+    }
 
 }
