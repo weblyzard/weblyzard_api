2334350bb2b51f1805c65121ff5b9dd1735f9913
Rod Michael Coronel <rodmaykel@gmail.com>
2019-01-07 17:28:15 +0100

    chg: remove default OGER_API_URL

--- src/python/weblyzard_api/client/__init__.py
+++ src/python/weblyzard_api/client/__init__.py
@@ -12,7 +12,7 @@ WEBLYZARD_API_URL = getenv("WEBLYZARD_API_URL") or "http://localhost:8080"
 WEBLYZARD_API_USER = getenv("WEBLYZARD_API_USER")
 WEBLYZARD_API_PASS = getenv("WEBLYZARD_API_PASS")
 
-OGER_API_URL = getenv("OGER_API_URL") or 'https://pub.cl.uzh.ch/projects/ontogene/medmon-oger/'
+OGER_API_URL = getenv("OGER_API_URL")
 
 
 
