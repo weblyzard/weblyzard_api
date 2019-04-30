1e99a40fbd85ed41e0a8c9fbfd4f40f8e4e6fbec
pavel <pavellippov@gmail.com>
2019-04-30 16:03:26 +0200

    new: test for dutch content

--- src/python/weblyzard_api/tests/client/test_jeremia_ng.py
+++ src/python/weblyzard_api/tests/client/test_jeremia_ng.py
@@ -13,14 +13,15 @@ from weblyzard_api.client.jeremia_ng import JeremiaNg
 class JeremiaTest(unittest.TestCase):
 
     DOCS = [{'id': content_id,
-             'body': 'Donald Trump and Barack Obama are presidents in the United States. Vienna is the capital of Austria, Berlin is the capital of Germany. Linz also is in Austria" {}'.format(content_id),
+             'body': 'Donald Trump and Barack Obama are presidents in the United States. Vienna is the capital of Austria, Berlin is the capital of Germany. Linz also is in Austria" {}'.format(
+                 content_id),
              'title': 'Hello "world" more ',
              'format': 'text/html',
              'header': {}} for content_id in xrange(1000, 1020)]
-
     def setUp(self):
-        service_url = 'localhost:63001'
+        service_url = 'gecko7.wu.ac.at:8081'
         self.client = JeremiaNg(url=service_url)
+
 
     def test_single_document_processing(self):
         """Test submitting a single document."""
@@ -30,6 +31,20 @@ class JeremiaTest(unittest.TestCase):
         from pprint import pprint
         pprint(result)
         self.assertTrue(result != "")
+
+    def test_dutch_string(self):
+        dutch_string = 'Bioscoopjournaals waarin Nederlandse onderwerpen van een bepaalde week worden gepresenteerd. Transport per vrachtauto van een 25 meter lange en 2,5 meter hoge ketel van de grens bij Nijmegen naar een kalkzandsteenfabriek bij Vuren bij Gorkum.'
+        doc = {'id': 1000,
+               'body': dutch_string,
+               'title': 'Hallo wereld',
+               'format': 'text/html',
+               'header': {}}
+        print('submitting document...')
+        result = self.client.submit_document(doc)
+
+        from pprint import pprint
+        pprint(result)
+        assert result['lang'] == 'NL'
 
 
 if __name__ == '__main__':
