05381390737f077ae41d05c6ec5c5d83d496a684
Katinka Boehm <boehm@weblyzard.com>
2019-05-21 18:13:57 +0200

    chg: expect new api success response in save_entity_batch

--- src/python/weblyzard_api/client/skb_rest_client.py
+++ src/python/weblyzard_api/client/skb_rest_client.py
@@ -163,7 +163,7 @@ class SKBRESTClient(object):
                                  data=json.dumps(entity_list),
                                  headers={'Content-Type': 'application/json'})
         if response.status_code < 400:
-            return json.loads(response.text)['uris']
+            return json.loads(response.text)['success']
         else:
             return None
 
