02bae7cb98233c56565d2700c4742eee95e1eb25
jakob.steixner <jakob.m.steixner@gmail.com>
2019-05-29 18:05:13 +0200

    fix: include dependency relations in sentences from Document.get_sentences

--- src/python/weblyzard_api/model/document.py
+++ src/python/weblyzard_api/model/document.py
@@ -304,11 +304,15 @@ class Document(object):
             pos_sequence = ' '.join([ts.pos for ts in token_spans])
             tok_sequence = ' '.join(
                 ['{},{}'.format(ts.start - offset, ts.end - offset) for ts in token_spans])
+            try:
+                dep_sequence = ' '.join(['{}:{}'.format(*ts.dependency.values()) for ts in token_spans])
+            except AttributeError:
+                dep_sequence = None
             value = self.get_text_by_span(sentence_span)
             result.append(Sentence(md5sum=sentence_span.md5sum,
                                    sem_orient=sentence_span.sem_orient,
                                    significance=sentence_span.significance,
                                    pos=pos_sequence, token=tok_sequence,
-                                   value=value, is_title=is_title))
+                                   value=value, is_title=is_title, dependency=dep_sequence))
 
         return result
