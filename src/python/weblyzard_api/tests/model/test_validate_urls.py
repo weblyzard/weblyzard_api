43309630de43d56fbb14fbe9099e65cb0b5000be
pavel <pavellippov@gmail.com>
2019-07-09 17:01:23 +0200

    chg: relations test

--- src/python/weblyzard_api/tests/model/test_validate_urls.py
+++ src/python/weblyzard_api/tests/model/test_validate_urls.py
@@ -1,6 +1,5 @@
 import unittest
 
-
 from wl_core_ng.models.document import JSON10ParserDocument
 
 
@@ -23,12 +22,70 @@ class TestValidURLs(unittest.TestCase):
             "uri": "/status/324623423432432325632",
             "title": "Tweet by Max Goebel"
         }
+        valid_relations = {
+
+            "content": "Puzzles and crosswords won't necessarily prevent Alzheimer's https://t.co/J8uojkURj4 via @NetDoctor",
+            "content_type": "text/plain",
+            "repository_id": "communidata.weblyzard.com/api",
+            "uri": "https://twitter.com/status/324623423432432325632",
+            "title": "Tweet by Max Goebel",
+            'relations': {'dc:references': ['http://google.com/path_to_file',
+                                            'https://gmail.com/path_to_mail'],
+                          'sioc:reply_of': 'https://gmail.com/random_thing'}
+        }
+
+        non_valid_relations_path_error = {
+
+            "content": "Puzzles and crosswords won't necessarily prevent Alzheimer's https://t.co/J8uojkURj4 via @NetDoctor",
+            "content_type": "text/plain",
+            "repository_id": "communidata.weblyzard.com/api",
+            "uri": "https://twitter.com/status/324623423432432325632",
+            "title": "Tweet by Max Goebel",
+            'relations': {
+                'dc:references': ['http://google.com',
+                                  'https://gmail.com/path_to_mail'],  # first url doesn't have path, not accepted
+                'sioc:reply_of': 'https://randomdomain.org/random_thing'
+            }
+        }
+        non_valid_relations_array_error = {
+
+            "content": "Puzzles and crosswords won't necessarily prevent Alzheimer's https://t.co/J8uojkURj4 via @NetDoctor",
+            "content_type": "text/plain",
+            "repository_id": "communidata.weblyzard.com/api",
+            "uri": "https://twitter.com/status/324623423432432325632",
+            "title": "Tweet by Max Goebel",
+            'relations': {
+                'dc:references': ['http://google.com/something',
+                                  'https://gmail.com/path_to_mail'],
+                'sioc:reply_of': 'https://random_domain.org/random_thing',
+                'key': {'urls': ['http://google.com/path_to_file',
+                                 'https://gmail.com/path_to_mail']}  # it should be array
+            }
+        }
+        non_valid_relations = {
+
+            "content": "Puzzles and crosswords won't necessarily prevent Alzheimer's https://t.co/J8uojkURj4 via @NetDoctor",
+            "content_type": "text/plain",
+            "repository_id": "communidata.weblyzard.com/api",
+            "uri": "https://twitter.com/status/324623423432432325632",
+            "title": "Tweet by Max Goebel",
+            'relations': {
+                'dc:references': ['http://google.com/something',
+                                  'https://gmail.com/path_to_mail'],
+                'sioc:reply_of': 'https:///random_thing'  # doesn't have domain
+            }
+        }
+
+        assert JSON10ParserDocument._validate_urls(valid_relations)
+
+        assert not JSON10ParserDocument._validate_urls(non_valid_relations)
+        assert not JSON10ParserDocument._validate_urls(non_valid_relations_array_error)
+        assert not JSON10ParserDocument._validate_urls(non_valid_relations_path_error)
 
         assert JSON10ParserDocument._validate_urls(valid_url)
+
         assert not JSON10ParserDocument._validate_urls(non_valid_url)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
