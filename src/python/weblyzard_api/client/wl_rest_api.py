19b0dcf5ccb01a2952745dddfea2d39573d8cbdc
max <mcgoebel@gmail.com>
2019-07-23 15:06:59 +0200

    new: check_active on compute_rest API client

--- src/python/weblyzard_api/client/wl_rest_api.py
+++ src/python/weblyzard_api/client/wl_rest_api.py
@@ -9,6 +9,9 @@ This module provides an easy client for the WL Document REST API.
 
 import json
 import requests
+import logging
+
+logger = logging.getLogger('weblyzard_api.client.wl_compute_rest_api_client')
 
 
 class WlComputeRestApiClient(object):
@@ -94,6 +97,16 @@ class WlComputeRestApiClient(object):
         """
         r = requests.get('/'.join([self.base_url, self.ENDPOINT_STATUS]))
         return r.json()
+
+    def check_active(self):
+        """ Check if the Compute API is up an healthy.
+        """
+        try:
+            r = requests.get('/'.join([self.base_url, self.ENDPOINT_STATUS])).status
+            return r.status_code == 200
+        except Exception as e:
+            logger.warn(e)
+        return False
 
 
 class WlSearchRestApiClient(object):
