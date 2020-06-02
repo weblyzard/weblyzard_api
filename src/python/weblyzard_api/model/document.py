8a163478175995c1696d40b1c01587c7a79f8195
max <mcgoebel@gmail.com>
2020-06-02 23:08:36 +0200

    chg: include_title -> true

--- src/python/weblyzard_api/model/document.py
+++ src/python/weblyzard_api/model/document.py
@@ -294,7 +294,7 @@ class Document(object):
                 return token_span.pos
         return None
 
-    def get_sentences(self, zero_based=False, include_title=False):
+    def get_sentences(self, zero_based=False, include_title=True):
         """
         Legacy method to extract webLyzard sentences from content model.
         :param zero_based: if True, enforce token indices starting at 0
