3955b7febdbed8460da2e4b578b71362b5e706e3
Max Goebel <mcgoebel@gmail.com>
2021-11-08 17:51:15 +0100

    chg: new fragments [breaking], upped version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,2 @@
-## weblyzard_api 0.2.2.1-SNAPSHOT
+## weblyzard_api 0.2.2.2-SNAPSHOT
 * Introduced new DocumentPartitions `FRAGMENT` and `DUPLICATE`, breaking change to all Java services/packages using `webLyzard_api`, please recompile
--- src/java/main/com/weblyzard/api/model/document/partition/DocumentPartition.java
+++ src/java/main/com/weblyzard/api/model/document/partition/DocumentPartition.java
@@ -7,5 +7,5 @@ package com.weblyzard.api.model.document.partition;
  *
  */
 public enum DocumentPartition {
-    TITLE, BODY, LAYOUT, LINE, SENTENCE, TOKEN, MISC, SENTIMENT_SCOPE, MULTIPLIER_SCOPE;
+    TITLE, BODY, LAYOUT, LINE, SENTENCE, TOKEN, MISC, FRAGMENT, DUPLICATE, SENTIMENT_SCOPE, MULTIPLIER_SCOPE;
 }
