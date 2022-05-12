e1c77b3f81e242a5b547a445dc79c624685ca62c
max <max@rigel>
2022-05-12 15:55:37 +0200

    new: xml_encoding of attributes optional

--- src/python/weblyzard_api/model/parsers/__init__.py
+++ src/python/weblyzard_api/model/parsers/__init__.py
@@ -470,11 +470,13 @@ class XMLParser(object):
         return relations
 
     @classmethod
-    def dump_xml_attributes(cls, attributes, mapping, resolve_namespaces=True):
+    def dump_xml_attributes(cls, attributes, mapping, resolve_namespaces=True,
+                            xml_encode_values=True):
         """
         :param attributes:
         :param mapping:
-        :param resovle_namespaces:
+        :param resolve_namespaces:
+        :param xml_encode_values:
         """
         new_attributes = {}
 
@@ -498,7 +500,10 @@ class XMLParser(object):
                     else:
                         key = '%s:%s' % (namespace, key)
             if value is not None and value not in ('None', 'null', '0.0'):
-                new_attributes[key] = cls.encode_value(value)
+                if xml_encode_values:
+                    new_attributes[key] = cls.encode_value(value)
+                else:
+                    new_attributes[key] = value
 
         return new_attributes
 
@@ -578,7 +583,8 @@ class XMLParser(object):
             attributes = cls.clean_attributes(attributes)
         except Exception as e:
             logger.warning(e)
-        root = etree.Element('{%s}page' % cls.get_default_ns(),
+        tag = "{{{0}}}page".format(cls.get_default_ns())
+        root = etree.Element(_tag=tag,
                              attrib=attributes,
                              nsmap=required_namespaces)
 
