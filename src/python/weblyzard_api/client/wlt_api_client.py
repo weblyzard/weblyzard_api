51e4f74314fa8bb529697cf07424eefeec636901
max <goebel@weblyzard.com>
2022-05-13 16:27:33 +0200

    Merge branch 'develop' of https://gitlab.semanticlab.net/core/weblyzard_api.git into develop

--- src/python/weblyzard_api/client/wlt_api_client.py
+++ src/python/weblyzard_api/client/wlt_api_client.py
@@ -60,7 +60,7 @@ class WltSearchRestApiClient(WltApiClient):
                          auth_token: str=None,
                          start_date: str=None, end_date: str=None,
                          count: int=10, offset: int=0,
-                         max_docs: int=-1,
+                         max_docs: int=-1, exact_match=True, similarity=70,
                          fields: List[str]=['document.contentid']):
         """ 
         Search an index for documents matching the search parameters.
@@ -105,10 +105,19 @@ class WltSearchRestApiClient(WltApiClient):
                     "should": []
             }}
             for term in terms:
-                term_query["bool"]["should"].append(
-                    {"text": {
-                        "phrase": term
-                    }})
+                if exact_match:
+                    term_query["bool"]["should"].append(
+                        {"text": {
+                            "phrase": term
+                        }})
+                else:
+                    term_query["bool"]["should"].append(
+                        {"text": {
+                            "similarto": {"value": term,
+                                          "options": {
+                                              "minmatching": {
+                                                  "percent": similarity}}}
+                        }})
 
             query["query"] = term_query
 
