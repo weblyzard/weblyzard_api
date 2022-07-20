533b9d1861b3c3de05a9aaf653be289cbdfb9c25
max <goebel@weblyzard.com>
2022-07-20 11:04:26 +0200

    rm: ewrt dependency

--- setup.py
+++ setup.py
@@ -24,8 +24,6 @@ setup(
                  'Programming Language :: Python :: 3.7',
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3 :: Only'],
-    dependency_links=[
-        'git+https://github.com/weblyzard/ewrt.git#egg=eWRT-0.9.1.12'],
     zip_safe=False,
     include_package_data=True,
     scripts=['src/python/weblyzard_api/client/openrdf/wl_upload_repository.py', ]
