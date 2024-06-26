7abf7f702436bcb382b37c8e49a374197dfbb503
Rod Michael Coronel <coronel@weblyzard.com>
2024-06-26 10:25:52 +0000

    Update file setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
                       'lxml',
                       'redis',
                       'requests',
+                      'urllib3==1.26.15', # temp to test effect on dependency resolution
                       'pytest',
                       'python-dotenv',
                       'sparqlwrapper'],
