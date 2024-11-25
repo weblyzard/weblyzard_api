874afe225eab009f2fe2e391e3e309d92b2d812e
Katinka Boehm <boehm@weblyzard.com>
2024-11-25 16:11:07 +0100

    chg(triplestore): improved prefix declaration in sparql queries
    do not send the full list of prefixes by default, making the query very large, but only inject any needed prefixes

--- src/python/weblyzard_api/tests/client/test_triplestore.py
+++ src/python/weblyzard_api/tests/client/test_triplestore.py
@@ -2,13 +2,14 @@ import unittest
 from os import getenv
 from pprint import pprint
 
+from weblyzard_api.client.triplestore import TriplestoreWrapper2
 from weblyzard_api.client.triplestore.blazegraph import BlazegraphWrapper
 from weblyzard_api.client.triplestore.fuseki import FusekiWrapper
 from weblyzard_api.client.triplestore.qlever import QleverWrapper
 
-
+FUSEKI_ENDPOINT = 'http://fuseki-lod.prod.i.weblyzard.net:8443/wikidata.20190603'
 class TriplestoreTestFuseki(unittest.TestCase):
-    FUSEKI_ENDPOINT = getenv('FUSEKI_ENDPOINT')
+    #FUSEKI_ENDPOINT = getenv('FUSEKI_ENDPOINT')
     if not FUSEKI_ENDPOINT:
         raise ValueError("No Fuseki query service endpoint set!")
     fuseki_wrapper = FusekiWrapper(sparql_endpoint=FUSEKI_ENDPOINT, debug=True)
@@ -38,6 +39,8 @@ class TriplestoreTestFuseki(unittest.TestCase):
 class TriplestoreTestBlazegraph(unittest.TestCase):
     # official wikidata endpoint
     blazegraph_wrapper = BlazegraphWrapper(f'https://query.wikidata.org/sparql')
+    #blazegraph_wrapper = BlazegraphWrapper(f'http://78.142.140.80:9999/bigdata/namespace/wdq')
+
 
     def test_query(self):
         query = '''
@@ -51,7 +54,6 @@ class TriplestoreTestBlazegraph(unittest.TestCase):
                             }
                     LIMIT 1000
                     '''
-
         bindings = self.blazegraph_wrapper.run_query(query)
         for result in self.blazegraph_wrapper.group_bindings(bindings):
             pprint(result)
@@ -115,3 +117,39 @@ class TriplestoreTestQlever(unittest.TestCase):
 
     def test_exists(self):
         assert (self.qlever_wrapper.exists(uri='http://www.wikidata.org/entity/Q76'))
+
+def test_ts_wrapper_get_deduplicated_prefixes():
+    query = '''
+        PREFIX wd: <http://www.wikidata.org/entity/>
+        SELECT ?class WHERE {
+            VALUES ?class { geonames:2779469 wd:Q76 wd:Q5}
+            ?class rdfs:label ?label.
+            ?class gn:parentFeature* ?class .
+            .
+    	}
+        '''
+
+    ts_wrapper = TriplestoreWrapper2(sparql_endpoint='dummy')
+
+    assert('PREFIX wd: <http://www.wikidata.org/entity/>' not in ts_wrapper.remove_duplicate_prefixes(query))
+    assert(ts_wrapper.remove_duplicate_prefixes(query,
+                                                prefixes='PREFIX wd: <http://www.wikidata.org/entity/>\n'
+                                                         'PREFIX geonames: <http://sws.geonames.org/>')
+           == 'PREFIX geonames: <http://sws.geonames.org/>')
+
+def test_ts_wrapper_get_only_used_prefixes():
+    query = '''
+    SELECT ?class WHERE {
+        VALUES ?class { geonames:2779469 wd:Q76 wd:Q5}
+        ?class rdfs:label ?label.
+        ?class gn:parentFeature* ?class .
+        .
+	}
+    '''
+    ts_wrapper = TriplestoreWrapper2(sparql_endpoint='dummy')
+    assert(ts_wrapper.retrieve_only_used_prefixes(query)
+           == 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n'
+              'PREFIX wd: <http://www.wikidata.org/entity/>\n'
+              'PREFIX geonames: <http://sws.geonames.org/>\n'
+              'PREFIX gn: <http://www.geonames.org/ontology#>'
+              )
