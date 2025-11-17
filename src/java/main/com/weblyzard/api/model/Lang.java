d0e0ef6786e3d4dc7c26b3b39243b0a6b159f7bd
Max Goebel <goebel@weblyzard.com>
2025-11-17 11:56:47 +0000

    Edit Lang.java: added CA and GR

--- src/java/main/com/weblyzard/api/model/Lang.java
+++ src/java/main/com/weblyzard/api/model/Lang.java
@@ -8,7 +8,7 @@ import java.util.Optional;
  * @author Norman Suesstrunk
  */
 public enum Lang {
-    ANY, DE, EN, FR, CS, ES, IT, AF, AR, BG, BN, DA, EL, ET, FA, FI, GU, HE, HI, HR, HU, ID, IS, JA, KN, KO, LT, LV, MK, ML, MR, NE, NL, NO, PA, PL, PT, RO, RU, SK, SL, SO, SQ, SV, SW, TA, TE, TH, TL, TR, UK, UR, VI;
+    ANY, DE, EN, FR, CS, ES, IT, CA, GR, AF, AR, BG, BN, DA, EL, ET, FA, FI, GU, HE, HI, HR, HU, ID, IS, JA, KN, KO, LT, LV, MK, ML, MR, NE, NL, NO, PA, PL, PT, RO, RU, SK, SL, SO, SQ, SV, SW, TA, TE, TH, TL, TR, UK, UR, VI;
 
     /**
      * Transforms a String with a language code into the corresponding Lang Enum.
