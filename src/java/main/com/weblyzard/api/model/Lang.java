aa84f99ed03b6a7101005996a303c8c35d9cfa57
max <goebel@weblyzard.com>
2026-03-04 21:08:29 +0100

    build: fix greek language iso to EL rather than GR

--- src/java/main/com/weblyzard/api/model/Lang.java
+++ src/java/main/com/weblyzard/api/model/Lang.java
@@ -4,17 +4,17 @@ import java.util.Optional;
 
 /**
  * Language codes supported by the webLyzard API.
- * 
+ *
  * @author Norman Suesstrunk
  */
 public enum Lang {
-    ANY, DE, EN, FR, CS, ES, IT, CA, GR, AF, AR, BG, BN, DA, EL, ET, FA, FI, GU, HE, HI, HR, HU, ID, IS, JA, KN, KO, LT, LV, MK, ML, MR, NE, NL, NO, PA, PL, PT, RO, RU, SK, SL, SO, SQ, SV, SW, TA, TE, TH, TL, TR, UK, UR, VI;
+    ANY, DE, EN, FR, CS, ES, IT, CA, AF, AR, BG, BN, DA, EL, ET, FA, FI, GU, HE, HI, HR, HU, ID, IS, JA, KN, KO, LT, LV, MK, ML, MR, NE, NL, NO, PA, PL, PT, RO, RU, SK, SL, SO, SQ, SV, SW, TA, TE, TH, TL, TR, UK, UR, VI;
 
     /**
      * Transforms a String with a language code into the corresponding Lang Enum.
-     * 
+     *
      * @param lang language String to be converted to a constant
-     * @return the Lang constant for the given language string (case insensitive)
+     * @return the Lang constant for the given language string (case-insensitive)
      */
     public static Optional<Lang> getLanguage(String lang) {
         try {
